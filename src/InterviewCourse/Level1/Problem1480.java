1c0205e4eaca318eaee65ebe5713f91200d3e017
VitBuk <vitbuk@gmail.com>
2023-03-11 09:50:50 +0200

    first commit

---
+++ src/InterviewCourse/Level1/Problem1480.java
@@ -0,0 +1,10 @@
+package InterviewCourse.Level1;
+
+public class Problem1480 {
+    public int[] runningSum(int[] nums) {
+        if (nums.length < 2)
+            return nums;
+
+            for (int i=1; )
+    }
+}
