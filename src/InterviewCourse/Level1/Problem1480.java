df14d5d504000c8b3dae1e16c8b532090cc4993e
VitBuk <vitbuk@gmail.com>
2023-03-11 10:00:02 +0200

    problem1480 solved

--- src/InterviewCourse/Level1/Problem1480.java
+++ src/InterviewCourse/Level1/Problem1480.java
@@ -1,10 +1,17 @@
 package InterviewCourse.Level1;
 
 public class Problem1480 {
-    public int[] runningSum(int[] nums) {
+    public static int[] runningSum(int[] nums) {
         if (nums.length < 2)
             return nums;
 
-            for (int i=1; )
+        int[] resultNums = new int[nums.length];
+        resultNums[0] = nums[0];
+
+            for (int i=1; i<nums.length; i++ ) {
+                resultNums[i] = resultNums[i-1] + nums[i];
+            }
+
+            return resultNums;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import InterviewCourse.Level1.Problem1480;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        int[] result = Problem1480.runningSum(array);
+
+        for (int i=0; i<array.length; i++) {
+            System.out.println(result[i]);
+        }
     }
 }
