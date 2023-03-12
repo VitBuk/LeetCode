14f196af55ad49c22167f20f5f32d7db00f9ca6c
VitBuk <vitbuk@gmail.com>
2023-03-12 20:40:37 +0200

    problem 205: 'isomorphic strings' solved

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 import InterviewCourse.Level1.Problem1480;
+import InterviewCourse.Level1.Problem205;
 import InterviewCourse.Level1.Problem724;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] array = {-1,-1,0,1,1,0};
-        System.out.println(Problem724.pivotIndex(array));
+        String s = "aabb";
+        String t = "ccdd";
+        String s2 = "abcdeee";
+        String t2 = "zxydfff";
+        String s3 = "abb";
+        String t3 = "abc";
+
+        System.out.println(Problem205.isIsomorphic(s,t));
+        System.out.println(Problem205.isIsomorphic(s2,t2));
+        System.out.println(Problem205.isIsomorphic(s3,t3));
+
     }
 }
